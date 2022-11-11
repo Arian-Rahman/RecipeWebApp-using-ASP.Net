82fa5f01df4912db30de59edc31551bdccffeaca
Arian Rahman <arian.rahman@northsouth.edu>
2022-11-11 12:41:17 +0600

    Add project files.

---
+++ Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+
+namespace RecipeWebApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; } //prop + tabx2
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Recipe() //ctor + tabx2
+        {
+
+        }
+    }
+}
